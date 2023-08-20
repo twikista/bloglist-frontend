3a0e004c2f61a5ec75062332b45c546d9ab3082e
Aaron Anama <twikista@gmail.com>
2023-08-20 22:28:40 +0100

    added blog slice and thunk

---
+++ src/features/blog/blogSplice.js
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { getBlogs, addBlog, deleteBlog, updateBlog } from './blogThunk'
+// import blogs from '../../services/blogs'
+
+const blogSplice = createSlice({
+  name: 'blogs',
+  initialState: {
+    blogs: [],
+    isLoading: false,
+  },
+  reducers: {},
+  extraReducers: {
+    [getBlogs.pending]: (state) => {
+      state.isLoading = true
+      // console.log(action)
+    },
+    [getBlogs.fulfilled]: (state, action) => {
+      state.isLoading = false
+      state.blogs = action.payload
+      // console.log(JSON.parse(JSON.stringify(state)))
+    },
+    [getBlogs.rejected]: (state) => {
+      state.isLoading = false
+      // console.log(action.payload)
+    },
+    [addBlog.fulfilled]: (state, action) => {
+      const { newBlog, user } = action.payload
+      state.blogs = state.blogs.concat({ ...newBlog, user })
+      // console.log(JSON.parse(JSON.stringify(state)))
+    },
+    [addBlog.rejected]: (state, action) => {
+      state.error = action.payload
+    },
+    [deleteBlog.fulfilled]: (state, action) => {
+      state.blogs = state.blogs.filter((blog) => blog.id !== action.payload)
+    },
+    [updateBlog.fulfilled]: (state, action) => {
+      const { returnedObject, user } = action.payload
+      state.blogs = state.blogs.map((blog) =>
+        blog.id !== returnedObject.id ? blog : { ...returnedObject, user: user }
+      )
+    },
+  },
+})
+
+export default blogSplice.reducer
---
+++ src/features/blog/blogThunk.js
@@ -0,0 +1,50 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import blogServices from '../../services/blogs'
+
+export const getBlogs = createAsyncThunk('blogs/getBlogs', async (thunkAPI) => {
+  try {
+    const blogs = await blogServices.getAll()
+    return blogs
+  } catch (error) {
+    return thunkAPI.rejectWithValue('data ftching failed')
+  }
+})
+
+export const addBlog = createAsyncThunk(
+  'blogs/addBlog',
+  async ({ newBlogObject, user }, thunkAPI) => {
+    try {
+      const newBlog = await blogServices.createNewBlog(newBlogObject)
+      return { newBlog, user }
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong')
+    }
+  }
+)
+
+export const deleteBlog = createAsyncThunk(
+  'blogs/deleteBlog',
+  async (blogId) => {
+    try {
+      await blogServices.deleteBlog(blogId)
+      return blogId
+    } catch (error) {
+      console.log(error)
+    }
+  }
+)
+
+export const updateBlog = createAsyncThunk(
+  'blogs/updateblog',
+  async ({ blogId, updatedBlogObject, user }) => {
+    try {
+      const returnedObject = await blogServices.updateBlog(
+        blogId,
+        updatedBlogObject
+      )
+      return { returnedObject, user }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+)
